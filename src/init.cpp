ad0328f44f984dbaa2d1aa91eed03f541e248a43
UASF <bitcoinuasf@protonmail.com>
2017-03-28 10:56:06 -0400

    Revert "Uasf in ua"

--- src/init.cpp
+++ src/init.cpp
@@ -1124,8 +1124,6 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
             return InitError(strprintf(_("User Agent comment (%s) contains unsafe characters."), cmt));
         uacomments.push_back(SanitizeString(cmt, SAFE_CHARS_UA_COMMENT));
     }
-    uacomments.push_back(CLIENT_VERSION_UA);
-    uacomments.push_back("BIP148");
     strSubVersion = FormatSubVersion(CLIENT_NAME, CLIENT_VERSION, uacomments);
     if (strSubVersion.size() > MAX_SUBVERSION_LENGTH) {
         return InitError(strprintf(_("Total length of network version string (%i) exceeds maximum length (%i). Reduce the number or size of uacomments."),
